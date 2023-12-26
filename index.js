e288aaa424c2292b1c322268efd7980b6b25cfd3
benni90100 <micelibenedetto800@gmail.com>
2023-12-26 14:50:43 +0100

    dev

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const currentHTML = document.querySelector("#current")
+const currenHourstHTML = document.querySelector("#current1")
 const input = document.querySelector("#locality")
 const btn = document.querySelector("#btn")
 async function getMeteoData() {
@@ -10,19 +11,42 @@ async function getMeteoData() {
     const city = location.name //città
     const localTime = location.localtime //data e ora odierna
     //ricaviamo le informazioni meteo attuali
-    console.log(meteoData.current)
-   //step due ricaviamo il primo layout contenente queste informazioni
-   const current = meteoData.current
-   const currentTemp = current.temp_c
-   const currentFeelsTemp = current.feelslike_c
+    // console.log(meteoData.current)
+    //step due ricaviamo il primo layout contenente queste informazioni
+    const current = meteoData.current
+    const currentTemp = current.temp_c
+    const currentFeelsTemp = current.feelslike_c
     const currentumidity = current.humidity
     const condition = current.condition
     const icon = condition.icon
-    console.log(icon);
+    // console.log(icon);
+    const previsioni = meteoData.forecast
+    const previsioniGiornaliere = previsioni.forecastday
+    
+    previsioniGiornaliere.forEach(giorni => {
+        const giornate = giorni.day
+        const condizioni = giornate.condition
+        const iconaCondizioni = condizioni.icon 
+        console.log(giornate)
+         
+        currenHourstHTML.innerHTML += `
+            <div class="flex justify-center align-center text-sm bg-slate-700/40 m-4 ">
+                <div class="flex justify-center"><img src="${iconaCondizioni}"/></div>
+                <div class="p-2">temp max <br/>${giornate.maxtemp_c}</div>
+                <div class="p-2">temp min <br>${giornate.mintemp_c}</div>
+                <div class="p-2">umidità <br>${giornate.avghumidity}</div>
+            </div>
+        
+    `
+        
+    });
+    
+
     //stampiamo in pagina tali dati
-    currentHTML.innerHTML=`
+
+    currentHTML.innerHTML = `
     <div
-            class="flex flex-col justify-center items-center bg-slate-800 w-3/5 h-screen  text-gray-400 text-center font-bold text-2xl"
+            class="flex flex-col justify-center items-center  w-3/5 h-screen  text-gray-400 text-center font-bold text-2xl"
             id="current">
             <div>
                 <h2 class="p-2 text-3xl">${city}</h2>
@@ -32,13 +56,14 @@ async function getMeteoData() {
             <div class="p-2"><img src="${icon}"/></div>
             <div class="flex">
                 <div class="p-2">temperatura <br/>${currentTemp}</div>
-                <div class="p-2">percepita <br>${ currentFeelsTemp }</div>
+                <div class="p-2">percepita <br>${currentFeelsTemp}</div>
                 <div class="p-2">umidità <br>${currentumidity}</div>
             </div>
         </div>
     `
+    
 
 }
 btn.addEventListener("click", getMeteoData)
-window.addEventListener("keypress", (e)=> e.key==="Enter" ? getMeteoData() : console.log(e))
+window.addEventListener("keypress", (e) => e.key === "Enter" ? getMeteoData() : console.log(e))
 
